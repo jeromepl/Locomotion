9366091789292515e9d64045689d9f68be5d9fae
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2018-06-03 21:51:11 -0700

    Add .bvh file parser

---
+++ bvhparser.py
@@ -0,0 +1,229 @@
+import re
+
+
+# Taken and adapted from https://github.com/20tab/bvh-python/blob/master/bvh.py
+
+class BvhNode:
+
+    def __init__(self, value=[], parent=None):
+        self.value = value
+        self.children = []
+        self.parent = parent
+        if self.parent:
+            self.parent.add_child(self)
+
+    def add_child(self, item):
+        item.parent = self
+        self.children.append(item)
+
+    def filter(self, key):
+        for child in self.children:
+            if child.value[0] == key:
+                yield child
+
+    def __iter__(self):
+        for child in self.children:
+            yield child
+
+    def __getitem__(self, key):
+        for child in self.children:
+            for index, item in enumerate(child.value):
+                if item == key:
+                    if index + 1 >= len(child.value):
+                        return None
+                    else:
+                        return child.value[index + 1:]
+        raise IndexError('key {} not found'.format(key))
+
+    def __repr__(self):
+        return str(' '.join(self.value))
+
+    @property
+    def name(self):
+        return self.value[1]
+
+
+class Bvh:
+
+    def __init__(self, data):
+        self.data = data
+        self.root = BvhNode()
+        self.frames = []
+        self.tokenize()
+
+    def tokenize(self):
+        first_round = []
+        accumulator = ''
+        for char in self.data:
+            if char not in ('\n', '\r'):
+                accumulator += char
+            elif accumulator:
+                    first_round.append(re.split('\\s+', accumulator.strip()))
+                    accumulator = ''
+        node_stack = [self.root]
+        frame_time_found = False
+        node = None
+        for item in first_round:
+            if frame_time_found:
+                self.frames.append(item)
+                continue
+            key = item[0]
+            if key == '{':
+                node_stack.append(node)
+            elif key == '}':
+                node_stack.pop()
+            else:
+                node = BvhNode(item)
+                node_stack[-1].add_child(node)
+            if item[0] == 'Frame' and item[1] == 'Time:':
+                frame_time_found = True
+
+    def search(self, *items):
+        found_nodes = []
+
+        def check_children(node):
+            if len(node.value) >= len(items):
+                failed = False
+                for index, item in enumerate(items):
+                    if node.value[index] != item:
+                        failed = True
+                        break
+                if not failed:
+                    found_nodes.append(node)
+            for child in node:
+                check_children(child)
+        check_children(self.root)
+        return found_nodes
+
+    def get_joints(self):
+        joints = []
+
+        def iterate_joints(joint):
+            joints.append(joint)
+            for child in joint.filter('JOINT'):
+                iterate_joints(child)
+        iterate_joints(next(self.root.filter('ROOT')))
+        return joints
+
+    def get_joints_names(self):
+        '''
+        Get the name of all joints in the model (like "Head", "Neck", etc.)
+        '''
+        joints = []
+
+        def iterate_joints(joint):
+            joints.append(joint.value[1])
+            for child in joint.filter('JOINT'):
+                iterate_joints(child)
+        iterate_joints(next(self.root.filter('ROOT')))
+        return joints
+
+    def get_joint_index(self, name):
+        return self.get_joints().index(self.get_joint(name))
+
+    def get_joint(self, name):
+        found = self.search('ROOT', name)
+        if not found:
+            found = self.search('JOINT', name)
+        if found:
+            return found[0]
+        raise LookupError('joint not found')
+
+    def joint_offset(self, name):
+        joint = self.get_joint(name)
+        offset = joint['OFFSET']
+        return (float(offset[0]), float(offset[1]), float(offset[2]))
+
+    def joint_channels(self, name):
+        '''
+        Get the name of all channels for a given joint name (like "Xposition", "Yposition", etc.)
+        '''
+        joint = self.get_joint(name)
+        return joint['CHANNELS'][1:]
+
+    def get_joint_channels_index(self, joint_name):
+        index = 0
+        for joint in self.get_joints():
+            if joint.value[1] == joint_name:
+                return index
+            index += int(joint['CHANNELS'][0])
+        raise LookupError('joint not found')
+
+    def frame_joint_channel(self, frame_index, joint, channel, value=None):
+        '''
+        Get the value of a channel of a joint at any given frame
+        '''
+        joint_index = self.get_joint_channels_index(joint)
+        channel_index = self.joint_channels(joint).index(channel)
+        if channel_index == -1 and value is not None:
+            return value
+        return float(self.frames[frame_index][joint_index + channel_index])
+
+    def frame_joint_channels(self, frame_index, joint, channels, value=None):
+        values = []
+        joint_index = self.get_joint_channels_index(joint)
+        for channel in channels:
+            channel_index = self.joint_channels(joint).index(channel)
+            if channel_index == -1 and value is not None:
+                values.append(value)
+            else:
+                values.append(
+                    float(
+                        self.frames[frame_index][joint_index + channel_index]
+                    )
+                )
+        return values
+
+    def frames_joint_channels(self, joint, channels, value=None):
+        all_frames = []
+        joint_index = self.get_joint_channels_index(joint)
+        for frame in self.frames:
+            values = []
+            for channel in channels:
+                channel_index = self.joint_channels(joint).index(channel)
+                if channel_index == -1 and value is not None:
+                    values.append(value)
+                else:
+                    values.append(
+                        float(frame[joint_index + channel_index]))
+            all_frames.append(values)
+        return all_frames
+
+    def joint_parent(self, name):
+        joint = self.get_joint(name)
+        if joint.parent == self.root:
+            return None
+        return joint.parent
+
+    def joint_parent_index(self, name):
+        joint = self.get_joint(name)
+        if joint.parent == self.root:
+            return -1
+        return self.get_joints().index(joint.parent)
+
+    @property
+    def nframes(self):
+        '''
+        Get the toal number of frames in the bvh file
+        '''
+        try:
+            return int(next(self.root.filter('Frames:')).value[1])
+        except StopIteration:
+            raise LookupError('number of frames not found')
+
+    @property
+    def frame_time(self):
+        try:
+            return float(next(self.root.filter('Frame')).value[2])
+        except StopIteration:
+            raise LookupError('frame time not found')
+
+
+if __name__ == "__main__":
+    bvh_file = open("walk-01-normal-azumi.bvh", "r")
+    bvh_contents = bvh_file.read()
+    bvh_file.close()
+    bvh = Bvh(bvh_contents)
+    # print(bvh.get_joints_names()) # Get the names of all joints in the model
+    # print(bvh.joint_channels("Head")) # Get the channels of a given joint
+    print(bvh.frame_joint_channel(0, "Head", "Xposition")) # Get the value of a channel (Xposition) of a joint (Head) at frame 0
